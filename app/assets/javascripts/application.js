541f65e092374c5e687b7b1f4e2f60000aa9a891
ilcomarian <ilcomarian@gmail.com>
2018-09-04 14:36:28 -0400

    sadasf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,7 @@
  
 //= require popper
 //= require bootstrap 
-let form;
+var form;
  
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -28,28 +28,28 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
-  let extract = ()=>{
-    let lName = document.getElementById("first-name")
+  // let extract = ()=>{
+  //   let lName = document.getElementById("first-name")
    
-    console.log(lName)
-    let last_name = document.getElementById("last_name")
-    let age = document.getElementById("age")
+  //   console.log(lName)
+  //   let last_name = document.getElementById("last_name")
+  //   let age = document.getElementById("age")
      
-    let cId = document.getElementById("cohort_id") 
-    var opt = cId.options[cId.selectedIndex];
+  //   let cId = document.getElementById("cohort_id") 
+  //   var opt = cId.options[cId.selectedIndex];
 
-    let rlName = document.getElementById("rfirst-name")
-    let rlast_name = document.getElementById("rlast_name")
-    let rage = document.getElementById("rage")
-    let rcId = $("#rcohort_id")
-    if(lName.value.length > 0){ rlName.value = lName.value;}
-   if(last_name.value.length > 0){
-    rlast_name.value = last_name.value
+  //   let rlName = document.getElementById("rfirst-name")
+  //   let rlast_name = document.getElementById("rlast_name")
+  //   let rage = document.getElementById("rage")
+  //   let rcId = $("#rcohort_id")
+  //   if(lName.value.length > 0){ rlName.value = lName.value;}
+  //  if(last_name.value.length > 0){
+  //   rlast_name.value = last_name.value
 
-   }
+  //  }
    
-  //  console.log("is !!!!!!" + opt.value )
-   console.log("is !!!!!!" + opt.text )
+  // //  console.log("is !!!!!!" + opt.value )
+  //  console.log("is !!!!!!" + opt.text )
    
-    $('#sub').click()
-  }
+  //   $('#sub').click()
+  // }
